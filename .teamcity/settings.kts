0c8866fc01fde8e57e8a952bcab246583c15c3af
Michael Akinde <michael.akinde@statnett.no>
2021-04-08 17:59:54 +0200

    Fix error in settings.kts
    Test Plan: N/A
    
    Reviewers: O5 elhub/devtools, sergei.prodanov
    
    Reviewed By: O5 elhub/devtools, sergei.prodanov
    
    Differential Revision: https://phabricator.elhub.cloud/D1673

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -13,6 +13,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.version
 import no.elhub.common.build.configuration.Assemble
 import no.elhub.common.build.configuration.AutoRelease
 import no.elhub.common.build.configuration.ProjectType
+import no.elhub.common.build.configuration.ProjectType.GRADLE
 import no.elhub.common.build.configuration.SonarScan
 import no.elhub.common.build.configuration.UnitTest
 import no.elhub.common.build.configuration.constants.GlobalTokens
@@ -40,9 +41,8 @@ project {
             SonarScan(
                 SonarScan.Config(
                     vcsRoot = DslContext.settingsRoot,
-                    sonarId = "no.elhub.tools:dev-tools-auto-release",
-                    sonarProjectSources = "src/main",
-                    sonarProjectTests = "src/test"
+                    type = GRADLE,
+                    sonarId = "no.elhub.tools:dev-tools-auto-release"
                 )
             )
         )
@@ -56,23 +56,22 @@ project {
             )
         )
 
+        val githubAuth = SshAgent({
+            teamcitySshKey = "teamcity_github_rsa"
+            param("secure:passphrase", GlobalTokens.githubSshPassphrase)
+        })
+
+
         buildType(
             AutoRelease(
                 AutoRelease.Config(
                     vcsRoot = DslContext.settingsRoot,
-                    type = ProjectType.GRADLE
+                    type = ProjectType.GRADLE,
+                    sshAgent = githubAuth
                 )
             ) {
+
                 VcsTrigger ()
-
-                features {
-
-                    sshAgent {
-                        teamcitySshKey = "teamcity_github_rsa"
-                        param("secure:passphrase", GlobalTokens.githubSshPassphrase)
-                    }
-
-                }
 
             })
 
