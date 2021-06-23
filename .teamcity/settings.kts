174a0b8ea5d9f9dcf9109680b20f5434e8008b43
Michael Akinde <michael.akinde@statnett.no>
2021-06-23 19:40:55 +0200

    Update to use elhub-gradle
    Summary: - [patch] update
    
    Test Plan:
    - ./gradlew clean test
    
    Reviewers: O5 elhub/devtools, sergei.prodanov
    
    Reviewed By: O5 elhub/devtools, sergei.prodanov
    
    Subscribers: teamcity
    
    Differential Revision: https://phabricator.elhub.cloud/D2378

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -18,6 +18,8 @@ version = "2020.2"
 project {
 
     val projectId = "no.elhub.tools:dev-tools-auto-release"
+    val projectType = ProjectType.GRADLE
+    val artifactoryRepository = "elhub-bin-release-local"
 
     params {
         param("teamcity.ui.settings.readOnly", "true")
@@ -29,7 +31,7 @@ project {
             UnitTest(
                 UnitTest.Config(
                     vcsRoot = DslContext.settingsRoot,
-                    type = ProjectType.GRADLE
+                    type = projectType
                 )
             )
         )
@@ -38,7 +40,7 @@ project {
             SonarScan(
                 SonarScan.Config(
                     vcsRoot = DslContext.settingsRoot,
-                    type = ProjectType.GRADLE,
+                    type = projectType,
                     sonarId = projectId
                 )
             )
@@ -48,7 +50,7 @@ project {
             Assemble(
                 Assemble.Config(
                     vcsRoot = DslContext.settingsRoot,
-                    type = ProjectType.GRADLE
+                    type = projectType
                 )
             )
         )
@@ -62,7 +64,8 @@ project {
             AutoRelease(
                 AutoRelease.Config(
                     vcsRoot = DslContext.settingsRoot,
-                    type = ProjectType.GRADLE,
+                    type = projectType,
+                    repository = artifactoryRepository,
                     sshAgent = githubAuth
                 )
             ) {
@@ -83,7 +86,7 @@ project {
         CodeReview(
             CodeReview.Config(
                 vcsRoot = DslContext.settingsRoot,
-                type = ProjectType.GRADLE,
+                type = projectType,
                 sonarId = projectId
             )
         )
