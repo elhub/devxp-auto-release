0644e075d2f745768cbc026967edc1c9371ff4fb
Sergii Prodan <serpro@disroot.org>
2022-04-24 11:30:26 +0200

    Add option for manual version bump
    Release [minor]

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,11 @@ val kotestVersion = "4.4.1"
 val jgitVersion = "5.11.0.202103091610-r"
 val mockkVersion = "1.10.6"
 
+repositories {
+    mavenCentral()
+    maven("https://jfrog.elhub.cloud/artifactory/elhub-mvn/")
+}
+
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
@@ -12,6 +17,7 @@ dependencies {
     implementation("info.picocli:picocli:4.6.1")
     implementation("org.slf4j:slf4j-api:1.7.30")
     implementation("org.slf4j:slf4j-simple:1.7.30")
+    implementation("io.github.serpro69:semver.kt-release:0.4.0")
     testImplementation("commons-io:commons-io:2.8.0")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-extensions-allure-jvm:$kotestVersion")
@@ -32,7 +38,7 @@ val fatJar = task("fatJar", type = Jar::class) {
         attributes["Implementation-Version"] = rootProject.version
         attributes["Main-Class"] = mainClassName
     }
-    from(configurations.compileClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
     exclude("META-INF/*.SF", "META-INF/*.DSA", "META-INF/*.RSA")
     with(tasks.jar.get() as CopySpec)
     mustRunAfter(tasks["jar"])
--- src/main/kotlin/no/elhub/tools/autorelease/AutoRelease.kt
+++ src/main/kotlin/no/elhub/tools/autorelease/AutoRelease.kt
@@ -1,14 +1,18 @@
 package no.elhub.tools.autorelease
 
+import io.github.serpro69.semverkt.release.Increment
+import io.github.serpro69.semverkt.release.SemverRelease
+import io.github.serpro69.semverkt.release.configuration.PropertiesConfiguration
 import no.elhub.tools.autorelease.log.Logger
 import no.elhub.tools.autorelease.project.ProjectType
 import no.elhub.tools.autorelease.project.ProjectType.ANSIBLE
-import no.elhub.tools.autorelease.project.VersionBump
 import no.elhub.tools.autorelease.project.VersionFile
-import no.elhub.tools.autorelease.project.VersionedRepository
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.api.ResetCommand
 import picocli.CommandLine
 import java.io.File
 import java.nio.file.Paths
+import java.util.*
 import java.util.concurrent.Callable
 import java.util.concurrent.TimeUnit
 import kotlin.system.exitProcess
@@ -41,6 +45,16 @@ class AutoRelease : Callable<Int> {
     private var project: ProjectType = ProjectType.GENERIC
 
     @CommandLine.Option(
+        names = ["-i", "--increment"],
+        description = [
+            "Version component to bump",
+            "Use one of: \${COMPLETION-CANDIDATES}.",
+            "Default: \${DEFAULT-VALUE}."
+        ]
+    )
+    private var increment: Increment = Increment.NONE
+
+    @CommandLine.Option(
         names = ["-e", "--extra-param"],
         description = [
             "Extra parameter for the gradle/maven/npm publish task."
@@ -60,44 +74,98 @@ class AutoRelease : Callable<Int> {
     )
     private var verboseMode: Array<Boolean> = emptyArray()
 
+    @CommandLine.Option(
+        names = ["--no-publish"],
+        description = [
+            "Run the publish command after creating a new version",
+        ],
+        negatable = true,
+        required = false,
+    )
+    private var publish: Boolean = true
+
+    @CommandLine.Option(
+        names = ["--dry-run"],
+        description = [
+            "Compute the next version without doing anything else",
+        ],
+        required = false,
+    )
+    private var dryRun: Boolean = false
+
     override fun call(): Int {
         val log = Logger(verboseMode)
         log.info("Processing a project of type $project...")
-        val repository = VersionedRepository(Paths.get(path).toFile())
-        log.info("Current version: ${repository.currentVersion}")
-        log.info("Unprocessed messages: ${repository.untaggedMessages.size}")
-        val currentVersion = repository.currentVersion
-        val increaseVersion = VersionBump.analyze(repository.untaggedMessages)
-        log.info("Setting version...")
-        val nextVersion = currentVersion.increase(increaseVersion)
-        val nextVersionString = if (nextVersion != currentVersion) {
-            nextVersion
-        } else { // Minor bump and add snapshot
-            "${currentVersion.increase(VersionBump.MINOR)}-SNAPSHOT"
+        val props = Properties().also {
+            it["git.repo.directory"] = Paths.get(path)
         }
-        log.info("Next version: $nextVersionString")
-        project.versionRegex?.let {
-            VersionFile.setVersion(
-                Paths.get(project.configFilePath),
-                project,
-                String.format(project.versionFormat, nextVersionString)
-            )
-        }
-        return if (nextVersion != currentVersion) {
-            if (project == ANSIBLE) repository.commit(project.configFilePath, "Release v$nextVersionString")
-            repository.setTag("v$nextVersionString")
-            if (project.publishCommand.isNotEmpty()) {
-                log.info("Publish release...")
-                val proc = Proc(File(path), Logger(verboseMode))
-                val cmd = proc.runCommand("${project.publishCommand} ${extraParams.joinToString(" ")}".trim()).also {
-                    it.waitFor(180, TimeUnit.SECONDS)
-                    log.info(it.inputStreamAsText())
+        val config = PropertiesConfiguration(props)
+        with(SemverRelease(config)) {
+            val latestVersion = currentVersion()
+            log.info("Current version: $latestVersion")
+            val increaseVersion = with(nextIncrement()) {
+                if (increment !in listOf(Increment.DEFAULT, Increment.NONE) && this != Increment.NONE) increment else this
+            }
+            log.info("Setting next $increaseVersion version...")
+            val nextVersion = when (increaseVersion) {
+                Increment.MAJOR, Increment.MINOR, Increment.PATCH -> release(increaseVersion)
+                Increment.PRE_RELEASE -> {
+                    latestVersion?.preRelease?.let {
+                        release(increaseVersion)
+                    } ?: createPreRelease(Increment.DEFAULT)
                 }
-                cmd.exitValue()
-            } else 0
-        } else {
-            log.debug("Nothing to do: nextVersion == currentVersion. Exiting...")
-            0
+                Increment.DEFAULT, Increment.NONE -> latestVersion
+            }
+            log.info("Next version: $nextVersion")
+            return if (!dryRun && nextVersion != latestVersion) {
+                project.versionRegex?.let {
+                    log.info("Set next version in ${project.configFilePath}...")
+                    VersionFile.setVersion(
+                        Paths.get(project.configFilePath),
+                        project,
+                        String.format(project.versionFormat, nextVersion)
+                    )
+                }
+                val repo = Git.open(Paths.get(path).toFile())
+                if (project == ANSIBLE) repo.commit(project.configFilePath, "Release v$nextVersion")
+                repo.setTag("v$nextVersion")
+                if (publish && project.publishCommand.isNotEmpty()) {
+                    log.info("Publish release...")
+                    val proc = Proc(File(path), Logger(verboseMode))
+                    val cmd =
+                        proc.runCommand("${project.publishCommand} ${extraParams.joinToString(" ")}".trim()).also {
+                            it.waitFor(180, TimeUnit.SECONDS)
+                            log.info(it.inputStreamAsText())
+                        }
+                    cmd.exitValue()
+                } else 0
+            } else {
+                log.debug("Nothing to do. Exiting...")
+                0
+            }
+        }
+    }
+
+    private fun Git.setTag(tagName: String) {
+        Git(repository).use { git ->
+            git.tag().setName(tagName).setMessage(tagName).setAnnotated(true).call()
+            //git.push().setTransportConfigCallback(SshConfig(SSH_FILE_PATH, null)).setPushTags().call()
+        }
+    }
+
+    private fun Git.commit(file: String, msg: String) {
+        Git(repository).use { git ->
+            git.reset() // unstage changes if any
+                .setRef("HEAD")
+                .setMode(ResetCommand.ResetType.MIXED)
+                .call()
+
+            git.add().addFilepattern(file).call()
+
+            git.commit()
+                .setMessage(msg) // QUESTION should we have a default msg for commits?
+                .setAuthor("auto-release", "auto-release@elhub.cloud") // TODO make author details configurable
+                .call()
         }
     }
 }
