56543b78ad2f2f31317d063c20805486dd6f74a7
Sergii Prodan <serpro@disroot.org>
2022-04-26 14:18:41 +0200

    Bump semver dependency
    [patch]

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ val jgitVersion = "5.11.0.202103091610-r"
 val mockkVersion = "1.10.6"
 
 repositories {
+    mavenCentral()
     maven("https://jfrog.elhub.cloud/artifactory/elhub-mvn/")
 }
 
@@ -16,7 +17,7 @@ dependencies {
     implementation("info.picocli:picocli:4.6.1")
     implementation("org.slf4j:slf4j-api:1.7.30")
     implementation("org.slf4j:slf4j-simple:1.7.30")
-    implementation("io.github.serpro69:semver.kt-release:0.4.0")
+    implementation("io.github.serpro69:semver.kt-release:0.4.1")
     testImplementation("commons-io:commons-io:2.8.0")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-extensions-allure-jvm:$kotestVersion")
