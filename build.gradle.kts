d89174943161b69f854cf10367f5106200467c44
Michael Akinde <michael.akinde@statnett.no>
2022-09-09 20:00:25 +0200

    Updated gradle config to use elhub-gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,6 @@
-import com.adarshr.gradle.testlogger.theme.ThemeType
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jfrog.gradle.plugin.artifactory.dsl.ArtifactoryPluginConvention
-import org.jfrog.gradle.plugin.artifactory.dsl.PublisherConfig
-import org.jfrog.gradle.plugin.artifactory.dsl.ResolverConfig
-
 plugins {
-    kotlin("jvm") version "1.6.20"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.20"
-    id("com.jfrog.artifactory") version "4.18.3"
-    id("maven-publish") apply true
-    id("com.github.ben-manes.versions") version "0.36.0"
-    id("jacoco")
-    id("com.adarshr.test-logger") version "2.1.1"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("application")
 }
 
 description = "Implement automated semantic release for gradle, maven and ansible projects."
@@ -19,10 +8,6 @@ description = "Implement automated semantic release for gradle, maven and ansibl
 val kotestVersion = "4.4.1"
 val jgitVersion = "5.11.0.202103091610-r"
 val mockkVersion = "1.10.6"
-
-repositories {
-    maven("https://jfrog.elhub.cloud/artifactory/elhub-mvn/")
-}
 
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
@@ -39,93 +24,18 @@ dependencies {
     testImplementation("io.mockk:mockk:$mockkVersion")
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+/*
+ * Executable Jar File Assembly.
+ */
+ val applicationMainClass : String by project
+
+application {
+    mainClass.set(applicationMainClass)
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = "1.8"
-        javaParameters = true
-    }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-    testLogging {
-        events("passed", "skipped", "failed")
-        showStandardStreams = true
-    }
-}
-
-jacoco {
-    toolVersion = "0.8.7" // Has to be the same as TeamCity
-}
-
-tasks.test {
-    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
-}
-
-tasks.jacocoTestReport {
-    dependsOn(tasks.test) // tests are required to run before generating the report
-}
-
-testlogger {
-    theme = ThemeType.MOCHA
-}
-
-
-val mainClassName = "no.elhub.tools.autorelease.AutoReleaseKt"
-
-val fatJar = task("fatJar", type = Jar::class) {
+val shadowJar by tasks.getting(com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar::class) {
     archiveBaseName.set(rootProject.name)
-    manifest {
-        attributes["Implementation-Title"] = rootProject.name
-        attributes["Implementation-Version"] = rootProject.version
-        attributes["Main-Class"] = mainClassName
-    }
-    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
-    exclude("META-INF/*.SF", "META-INF/*.DSA", "META-INF/*.RSA", "*.html")
-    duplicatesStrategy = DuplicatesStrategy.INCLUDE
-    with(tasks.jar.get() as CopySpec)
-    mustRunAfter(tasks["jar"])
+    archiveClassifier.set("")
 }
 
-tasks["assemble"].dependsOn(tasks["fatJar"])
-
-val mavenPubName = "mavenJavaLibrary"
-
-publishing {
-    publications {
-        create<MavenPublication>(mavenPubName) {
-            from(components["java"])
-        }
-    }
-}
-
-fun Project.artifactory(configure: ArtifactoryPluginConvention.() -> Unit): Unit =
-    configure(project.convention.getPluginByName("artifactory"))
-
-artifactory {
-    setContextUrl("https://jfrog.elhub.cloud/artifactory")
-    publish(delegateClosureOf<PublisherConfig> {
-        repository(delegateClosureOf<groovy.lang.GroovyObject> {
-            setProperty("repoKey", project.findProperty("artifactoryRepository") ?: "elhub-mvn-dev-local")
-            setProperty("username", project.findProperty("artifactoryUsername") ?: "nouser")
-            setProperty("password", project.findProperty("artifactoryPassword") ?: "nopass")
-        })
-        defaults(delegateClosureOf<groovy.lang.GroovyObject> {
-            invokeMethod("publications", mavenPubName)
-            setProperty("publishArtifacts", true)
-            setProperty("publishPom", true)
-        })
-    })
-    resolve(delegateClosureOf<ResolverConfig> {
-        setProperty("repoKey", "repo")
-    })
-}
-
-tasks["artifactoryPublish"].dependsOn(tasks["assemble"])
-
-tasks["publish"].dependsOn(tasks["artifactoryPublish"])
+tasks["assemble"].dependsOn(tasks["shadowJar"])
