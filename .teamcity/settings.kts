afc7db958233c8593be5aada37695d3a8aa998f1
Michael Akinde <michael.akinde@statnett.no>
2024-06-10 15:10:07 +0200

    Updated the pipelines to build-config v0.103.0

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,13 +1,25 @@
 import no.elhub.devxp.build.configuration.pipeline.ElhubProject.Companion.elhubProject
-import no.elhub.devxp.build.configuration.pipeline.constants.Group.DEVXP
+import no.elhub.devxp.build.configuration.pipeline.constants.DeployEnvironment.Environment.TEST_11
+import no.elhub.devxp.build.configuration.pipeline.constants.DeployEnvironment.Environment.TEST_13
+import no.elhub.devxp.build.configuration.pipeline.constants.Group.CORE
+import no.elhub.devxp.build.configuration.pipeline.constants.JavaVersion.VERSION_1_8
+import no.elhub.devxp.build.configuration.pipeline.jobs.deploy
 import no.elhub.devxp.build.configuration.pipeline.jobs.gradleAutoRelease
+import no.elhub.devxp.build.configuration.pipeline.jobs.gradleIntegrationTest
 import no.elhub.devxp.build.configuration.pipeline.jobs.gradleVerify
 
-elhubProject(DEVXP, "devxp-auto-release") {
-    pipeline(withReleaseVersion = false) {
+elhubProject(CORE, "elhub-cp-ami-adapter") {
+
+    pipeline {
         sequential {
-            gradleVerify()
-            //gradleAutoRelease(artifacts = listOf(verifyArtifacts))
+            val artifacts = gradleVerify()
+            /*
+            sequential {
+                gradleAutoRelease(artifacts = listOf(artifacts)) {
+                    javaVersion = VERSION_1_8
+                }
+            }
+            */
         }
     }
 }
