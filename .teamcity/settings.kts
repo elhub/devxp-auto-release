0f8a7c58145414eda2801f1869be5e91fd8f21aa
Michael Akinde <michael.akinde@statnett.no>
2024-06-10 15:19:51 +0200

    Fix the IDs

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -8,18 +8,12 @@ import no.elhub.devxp.build.configuration.pipeline.jobs.gradleAutoRelease
 import no.elhub.devxp.build.configuration.pipeline.jobs.gradleIntegrationTest
 import no.elhub.devxp.build.configuration.pipeline.jobs.gradleVerify
 
-elhubProject(CORE, "elhub-cp-ami-adapter") {
+elhubProject(DEVXP, "devxp-build-configuration") {
 
     pipeline {
         sequential {
             val artifacts = gradleVerify()
-            /*
-            sequential {
-                gradleAutoRelease(artifacts = listOf(artifacts)) {
-                    javaVersion = VERSION_1_8
-                }
-            }
-            */
+            gradleAutoRelease(artifacts = listOf(artifacts))
         }
     }
 }
