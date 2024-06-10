0c43c94689faf307eff7dd769a68305835e02730
Michael Akinde <michael.akinde@statnett.no>
2024-06-10 15:21:33 +0200

    Fix the imports in settings,kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,11 +1,6 @@
 import no.elhub.devxp.build.configuration.pipeline.ElhubProject.Companion.elhubProject
-import no.elhub.devxp.build.configuration.pipeline.constants.DeployEnvironment.Environment.TEST_11
-import no.elhub.devxp.build.configuration.pipeline.constants.DeployEnvironment.Environment.TEST_13
-import no.elhub.devxp.build.configuration.pipeline.constants.Group.CORE
-import no.elhub.devxp.build.configuration.pipeline.constants.JavaVersion.VERSION_1_8
-import no.elhub.devxp.build.configuration.pipeline.jobs.deploy
+import no.elhub.devxp.build.configuration.pipeline.constants.Group.DEVXP
 import no.elhub.devxp.build.configuration.pipeline.jobs.gradleAutoRelease
-import no.elhub.devxp.build.configuration.pipeline.jobs.gradleIntegrationTest
 import no.elhub.devxp.build.configuration.pipeline.jobs.gradleVerify
 
 elhubProject(DEVXP, "devxp-build-configuration") {
