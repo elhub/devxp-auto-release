67153093a92d86626cd048f55e2a1e137a090edc
Michael Akinde <michael.akinde@statnett.no>
2024-06-13 12:50:03 +0200

    Update artifactory repository [patch]

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,16 +1,20 @@
 import no.elhub.devxp.build.configuration.pipeline.ElhubProject.Companion.elhubProject
 import no.elhub.devxp.build.configuration.pipeline.constants.Group.DEVXP
+import no.elhub.devxp.build.configuration.pipeline.constants.ArtifactoryRepository
 import no.elhub.devxp.build.configuration.pipeline.jobs.gradleAutoRelease
 import no.elhub.devxp.build.configuration.pipeline.jobs.gradleVerify
 
 elhubProject(DEVXP, "devxp-auto-release") {
+    val artifactoryRepository = ArtifactoryRepository.ELHUB_BIN_RELEASE_LOCAL
 
     pipeline {
         sequential {
             val artifacts = gradleVerify {
                 analyzeDependencies = false
             }
-            gradleAutoRelease(artifacts = listOf(artifacts))
+            gradleAutoRelease(artifacts = listOf(artifacts)) {
+                repository = artifactoryRepository
+            }
         }
     }
 }
