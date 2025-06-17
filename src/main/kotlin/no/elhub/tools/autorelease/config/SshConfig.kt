58200dbdd816d9cd0019669c97d0ca275e2735f9
Michael Akinde <michael.akinde@statnett.no>
2025-06-17 14:49:07 +0200

    Fix jgit upgrade
    Upgrading jgit from v5 to v7 broke the build. This update should fix it.
    
    ## 📋 Checklist
    
    * ✅ Lint checks passed on local machine.
    * ✅ Unit tests passed on local machine.

--- src/main/kotlin/no/elhub/tools/autorelease/config/SshConfig.kt
+++ src/main/kotlin/no/elhub/tools/autorelease/config/SshConfig.kt
@@ -3,10 +3,10 @@ package no.elhub.tools.autorelease.config
 import com.jcraft.jsch.JSch
 import com.jcraft.jsch.Session
 import org.eclipse.jgit.api.TransportConfigCallback
-import org.eclipse.jgit.transport.JschConfigSessionFactory
-import org.eclipse.jgit.transport.OpenSshConfig
 import org.eclipse.jgit.transport.SshTransport
 import org.eclipse.jgit.transport.Transport
+import org.eclipse.jgit.transport.ssh.jsch.JschConfigSessionFactory
+import org.eclipse.jgit.transport.ssh.jsch.OpenSshConfig
 import org.eclipse.jgit.util.FS
 
 /**
@@ -14,13 +14,15 @@ import org.eclipse.jgit.util.FS
  */
 class SshConfig(val sshKeyPath: String, val sshPassPhrase: String?) : TransportConfigCallback {
     override fun configure(transport: Transport?) {
-        (transport as SshTransport).sshSessionFactory = object : JschConfigSessionFactory() {
-            override fun configure(hc: OpenSshConfig.Host?, session: Session?) {
-                session?.setConfig("StrictHostKeyChecking", "no")
-            }
+        if (transport is SshTransport) {
+            transport.sshSessionFactory = object : JschConfigSessionFactory() {
+                override fun configure(hc: OpenSshConfig.Host?, session: Session?) {
+                    session?.setConfig("StrictHostKeyChecking", "no")
+                }
 
-            override fun createDefaultJSch(fs: FS?): JSch = super.createDefaultJSch(fs).apply {
-                addIdentity(sshKeyPath, sshPassPhrase)
+                override fun createDefaultJSch(fs: FS?): JSch = super.createDefaultJSch(fs).apply {
+                    addIdentity(sshKeyPath, sshPassPhrase)
+                }
             }
         }
     }
